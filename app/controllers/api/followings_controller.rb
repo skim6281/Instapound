5e3bd005d7e10b140e103830c9a1e9d4eee8e3d0
skim6281 <skim6281@gmail.com>
2017-02-16 12:03:02 -0500

    Add Images Controller

---
+++ app/controllers/api/followings_controller.rb
@@ -0,0 +1,25 @@
+class Api::FollowingsController < ApplicationController
+  before_filter :require_login!
+
+  def create
+    @followings = Following.new(followings_params)
+    @followings.follower = current_user
+    if @followings.save
+      render 'api/images/index'
+    else
+      render json: @followings.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @followings = current_user.followings.find(params[:id])
+    @followings.destroy
+    render 'api/images/index'
+  end
+
+  private
+
+  def followings_params
+    params.require(:followings).permit(:followee)
+  end
+end
