c076110f20246894618caccb2763bb3a8f303031
skim6281 <skim6281@gmail.com>
2017-02-16 11:53:26 -0500

    Add user associations

--- app/models/following.rb
+++ app/models/following.rb
@@ -10,4 +10,15 @@
 #
 
 class Following < ApplicationRecord
+  validates :followee, :follower, presence: true
+  validates :follower, uniqueness: { scope: :followee }
+
+  belongs_to :followee,
+    class_name: "User",
+    foreign_key: :followee_id
+
+  belongs_to :follower,
+    class_name: "User",
+    foreign_key: :follower_id
+
 end
