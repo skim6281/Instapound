7bbd69d722495995e51bcb1ac7230addca30d3c0
skim6281 <skim6281@gmail.com>
2017-02-17 12:00:59 -0500

    Add styling and time ago

--- app/views/api/images/_image.json.jbuilder
+++ app/views/api/images/_image.json.jbuilder
@@ -2,4 +2,5 @@ json.id image.id
 json.author_name image.user.username
 json.location image.location
 json.caption image.caption
+json.uploaded_at image.created_at.to_s
 json.photo_url image.photo.url
--- frontend/components/photo_feed/post.jsx
+++ frontend/components/photo_feed/post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { time_ago } from '../../util/time_ago_util';
 
 class Post extends React.Component {
   constructor(props) {
@@ -10,10 +11,15 @@ class Post extends React.Component {
     return(
       <div className="post-container">
         <header className="post-header">
-          <img className="post-author-profile-pic" src={image.author_profile_pic_url}/>
-          <div className="post-author-name">
-            <a className="author-name text">{image.author_name}</a>
+          <div className="post-user">
+            <img className="post-author-profile-pic" src={image.author_profile_pic_url}/>
+            <div className="post-author-name">
+              <a className="author-name text">{image.author_name}</a>
+            </div>
           </div>
+          <a className="time-ago">
+            {time_ago(image.uploaded_at)}
+          </a>
         </header>
         <img className="post-image" src={image.photo_url}/>
         <footer className="post-footer">
