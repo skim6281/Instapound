19caa70d005bf28d42ab4f91a970f1602a0ef845
skim6281 <skim6281@gmail.com>
2017-02-18 15:39:41 -0500

    Add ProfileReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import PhotoFeedReducer from './photo_feed_reducer';
+import ProfileReducer from './profile_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
-  photoFeed: PhotoFeedReducer
+  photoFeed: PhotoFeedReducer,
+  profile: ProfileReducer
 });
 
 export default RootReducer;
