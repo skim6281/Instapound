9677f23026f59184a6abb3ae53082c283fb30979
skim6281 <skim6281@gmail.com>
2017-02-20 17:36:48 -0500

    Remove methods from window

--- frontend/instapound.jsx
+++ frontend/instapound.jsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import Root from './components/root';
 import configureStore from './store/store';
-import { logout } from './actions/session_actions';
-// import { fetchImages } from './actions/photo_feed_actions';
-import { fetchImages } from './util/photo_feed_api_util';
-import { fetchUser } from './util/profile_api_util';
-
-window.logout = logout;
-window.fetchImages = fetchImages;
-window.fetchUser = fetchUser;
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
