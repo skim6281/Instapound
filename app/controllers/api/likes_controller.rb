e7bbaa46e7f04173ce815bc2adbf837f2fd60d4f
skim6281 <skim6281@gmail.com>
2017-02-21 14:09:53 -0500

    Add create and destroy methods

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,2 +1,28 @@
 class Api::LikesController < ApplicationController
+  def create
+    @like = Like.new(like_params)
+    @like.user_id = current_user.id
+    if @like.save
+      @image = Image.find(@like.image_id)
+      render 'api/images/show'
+    else
+      render json: @like.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @like = Like.find_by(image_id: params[:id], user_id: current_user.id)
+    if @like.destroy
+      @image = Image.find(@like.image_id)
+      render 'api/images/show'
+    else
+      render json: @like.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:image_id, :user_id)
+  end
 end
