3f812fcedfe79f1c852c2e5a7693b256cad4322a
skim6281 <skim6281@gmail.com>
2017-02-21 13:37:55 -0500

    Add annotation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text             not null
+#  user_id    :integer          not null
+#  image_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ApplicationRecord
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  image_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Like < ApplicationRecord
 end
