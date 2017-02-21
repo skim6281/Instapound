eaa8e244c367b34a2ecaf34161ed190a40a2d607
skim6281 <skim6281@gmail.com>
2017-02-21 14:27:15 -0500

    Fix typo

--- app/models/like.rb
+++ app/models/like.rb
@@ -11,12 +11,12 @@
 
 class Like < ApplicationRecord
   validates :image, :liker, presence: true
-  validates :image, uniquness: { scope: :user }
+  validates :image, uniqueness: { scope: :user }
 
   belongs_to :image
 
   belongs_to :liker,
     class_name: "User",
     foreign_key: :user_id
-    
+
 end
