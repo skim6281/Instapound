51f13a7126bc853db35b17cc6a4b894433254512
skim6281 <skim6281@gmail.com>
2017-03-16 16:57:31 -0400

    Add loading reducer

---
+++ frontend/reducers/loading_reducer.js
@@ -0,0 +1,14 @@
+const initialState - {
+  imageLoading: false,
+  feedLoading: false
+}
+
+export default (state = initialState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case :
+
+    default:
+      return state;
+  }
+}
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import PhotoFeedReducer from './photo_feed_reducer';
 import ProfileReducer from './profile_reducer';
+import LoadingReducer from './loading_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   photoFeed: PhotoFeedReducer,
-  profile: ProfileReducer
+  profile: ProfileReducer,
+  loading: LoaingReducer
 });
 
 export default RootReducer;
