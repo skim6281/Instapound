cadf46b7b96eee76fe6bb417143015358341dc48
skim6281 <skim6281@gmail.com>
2017-03-28 16:17:08 -0400

    Add bio column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221182603) do
+ActiveRecord::Schema.define(version: 20170328201500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20170221182603) do
     t.string   "profile_pic_content_type"
     t.integer  "profile_pic_file_size"
     t.datetime "profile_pic_updated_at"
+    t.string   "bio"
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
