a683e1c39cffa178e3c8a6d24d6b42e09fe6fcd5
skim6281 <skim6281@gmail.com>
2017-03-28 23:30:09 -0400

    Add route to edit profile

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,6 +4,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import AuthFormContainer from './auth_form/auth_form_container';
 import ProfileContainer from './profile/profile_container';
 import PhotoFeedContainer from './photo_feed/photo_feed_container';
+import EditProfileContainer from './edit_profile/edit_profile_container';
 import App from './App';
 
 const Root = ({ store }) => {
@@ -21,7 +22,9 @@ const Root = ({ store }) => {
         <Route path="/" component={App}>
           <IndexRoute component={PhotoFeedContainer}/>
           <Route path=":username" component={ProfileContainer}/>
-
+          <Route path="/accounts/">
+            <Route path="edit" component={EditProfileContainer} />
+          </Route>
         </Route>
       </Router>
     </Provider>
