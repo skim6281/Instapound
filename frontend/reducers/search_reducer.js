bf5de30d37cb94405c95a8abceaa568ad4bfbaba
skim6281 <skim6281@gmail.com>
2017-05-24 13:59:08 -0400

    Add RECEIVE_USERS case

--- frontend/reducers/search_reducer.js
+++ frontend/reducers/search_reducer.js
@@ -1,2 +1,17 @@
 import {RECEIVE_USERS} from '../actions/user_actions';
-import merge from 'lodash/merge';
+
+const initialState = {
+  users: []
+}
+
+const SearchReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_USERS:
+      return Object.assign({}, state, {users: action.users})
+    default:
+      return state;
+  }
+};
+
+export default SearchReducer;
