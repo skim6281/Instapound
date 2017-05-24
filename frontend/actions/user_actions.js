10af343f4a231db04cf5feb02d98ca8ef98ff358
skim6281 <skim6281@gmail.com>
2017-05-24 17:56:23 -0400

    Edit data attribute

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,13 +1,18 @@
 import * as UserAPIUtil from '../util/user_api_util';
 
 export const RECEIVE_USERS = "RECEIVE_USERS";
+export const REMOVE_USERS = "REMOVE_USERS";
 
 export const receiveUsers = users => ({
   type: RECEIVE_USERS,
   users
 });
 
+export const removeUsers = () => ({
+  type: REMOVE_USERS
+});
+
 export const fetchUsers = (userName) => dispatch => {
   return UserAPIUtil.fetchUsers(userName)
     .then(users => dispatch(receiveUsers(users)));
-}
+};
