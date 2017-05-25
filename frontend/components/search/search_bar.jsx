e0c4ad008420d642080e49961e5f07b844d40061
skim6281 <skim6281@gmail.com>
2017-05-25 16:26:38 -0400

    Add renderUsersList method

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -5,15 +5,13 @@ class SearchBar extends React.Component {
     super(props);
     this.state = {query: "", active: false};
     this.update = this.update.bind(this);
-    this.clearQuery = this.clearQuery.bind(this);
   }
 
   update(e) {
     e.preventDefault();
     if(e.target.value === "") {
-      this.clearQuery();
       this.props.removeUsers();
-      this.setState({active: false});
+      this.setState({query: "", active: false});
     } else {
       this.setState({query: e.target.value, active: true},
         () => this.props.fetchUsers(this.state.query));
@@ -21,20 +19,26 @@ class SearchBar extends React.Component {
   }
 
   renderUsers() {
-    if (this.state.query !== "") {
-      return this.props.users.map(user => {
-        return (
-          <li className= 'user-li' key={user.id}>
-            <img src={user.profile_pic_url}/>
-            <span>{user.username}</span>
-          </li>
-        )
-      });
-    }
+    return this.props.users.map(user => {
+      return (
+        <li className= 'user-li' key={user.id}>
+          <img src={user.profile_pic_url}/>
+          <span>{user.username}</span>
+        </li>
+      )
+    });
   }
 
-  clearQuery() {
-    this.setState({query: ""});
+  renderUsersList() {
+    if (this.state.query !== "") {
+      return (
+        <div className='users-list-div'>
+          <ul className='users-list'>
+            {this.renderUsers()}
+          </ul>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -48,11 +52,7 @@ class SearchBar extends React.Component {
             onChange={this.update}
             value={this.state.query}/>
         </form>
-        <div className='users-list-div'>
-          <ul className='users-list'>
-            {this.renderUsers()}
-          </ul>
-        </div>
+        {this.renderUsersList()}
       </div>
     )
   }
