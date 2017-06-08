55b6b3ccf5bcb478036468a0d5b9261b8df3695a
skim6281 <skim6281@gmail.com>
2017-06-08 19:53:53 -0400

    Add fetchUser and updateUser methods

--- frontend/util/user_api_util.js
+++ frontend/util/user_api_util.js
@@ -5,3 +5,18 @@ export const fetchUsers = userName => {
     data: {userName: userName}
   });
 };
+
+export const fetchUser = username => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/users/${username}`
+  });
+};
+
+export const updateUser = user => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `api/users/${user.username}`,
+    data: { user }
+  });
+};
