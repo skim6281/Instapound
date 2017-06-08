a718198379ac56725777ae7002f003d9737ed99a
skim6281 <skim6281@gmail.com>
2017-06-08 19:43:26 -0400

    Remove unused methods

--- frontend/util/comment_api_util.js
+++ frontend/util/comment_api_util.js
@@ -12,18 +12,3 @@ export const deleteComment = commentId => {
     url: `api/comments/${commentId}`
   });
 };
-
-export const createImageComment = comment => {
-  return $.ajax({
-    method: "POST",
-    url: `api/images/${comment.image_id}/comments`,
-    data: { comment }
-  });
-};
-
-export const deleteImageComment = commentId => {
-  return $.ajax({
-    method: "DELETE",
-    url: `api/comments/${commentId}`
-  });
-};
